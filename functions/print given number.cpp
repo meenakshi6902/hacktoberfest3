0e6a7323bede1143136dae14ca6d94b9873c8a69
meenakshi_0602 <meenakshisharma6902@gmail.com>
2021-10-23 14:24:53 +0530

    DSA Programs

---
+++ functions/print given number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int num1){
+    cout<<num1<<endl;
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
